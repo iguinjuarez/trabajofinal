19f5228489110593b2e7332b8c7201f6e66271bf
iguinjuarez <iguin632@gmail.com>
2024-11-18 21:10:18 -0300

    Delete src directory

--- src/config/setupModel.js
+++
@@ -1,8 +0,0 @@
-const Students = require('../model/student');
-const {getInstance} = require('./setupDB');
-
-const setupModel = async () => {
-const instanceDb = await getInstance();
-Students.init(instanceDb);
-};
-setupModel();
